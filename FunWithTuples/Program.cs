33734cbe0398accd4292a4c5eb6f4d862b75b7a5
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-20 14:05:03 -0400

    Use tuples

--- FunWithTuples/Program.cs
+++ FunWithTuples/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FunWithTuples
 {
@@ -10,6 +6,21 @@ namespace FunWithTuples
     {
         static void Main(string[] args)
         {
+            (string, int, string) values = ("a", 5, "c");
+            (string FirstLetter, int TheNumber, string SecondLetter) valuesWithNames = ("a", 5, "c");
+
+            Console.WriteLine($"First item: {values.Item1}");
+            Console.WriteLine($"Second item: {values.Item2}");
+            Console.WriteLine($"Third item: {values.Item3}");
+            Console.WriteLine();
+
+            Console.WriteLine($"First item: {valuesWithNames.FirstLetter}");
+            Console.WriteLine($"Second item: {valuesWithNames.TheNumber}");
+            Console.WriteLine($"Third item: {valuesWithNames.SecondLetter}");
+            Console.WriteLine($"First item: {valuesWithNames.Item1}");
+            Console.WriteLine($"Second item: {valuesWithNames.Item2}");
+            Console.WriteLine($"Third item: {valuesWithNames.Item3}");
+            Console.ReadLine();
         }
     }
 }
