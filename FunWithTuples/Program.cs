bdccde40bfca5e3ba47bfac4b6e29c1e1c40a5fb
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-20 14:19:55 -0400

    Add Point struct with Deconstruct method.

--- FunWithTuples/Program.cs
+++ FunWithTuples/Program.cs
@@ -4,6 +4,22 @@ namespace FunWithTuples
 {
     class Program
     {
+
+        struct Point
+        {
+            // Fields of the structure.
+            public int X;
+            public int Y;
+
+            // A custom constructor.
+            public Point(int XPos, int YPos)
+            {
+                X = XPos;
+                Y = YPos;
+            }
+
+            public (int XPos, int YPos) Deconstruct() => (X, Y);
+        }
         static void Main(string[] args)
         {
             (string, int, string) values = ("a", 5, "c");
@@ -11,6 +27,8 @@ namespace FunWithTuples
             var foo = new { Prop1 = "first", Prop2 = "second" };
             var bar = (foo.Prop1, foo.Prop2);
             var samples = FillTheseValues();
+            Point p = new Point(7, 5);
+            var pointValues = p.Deconstruct();
 
             Console.WriteLine($"First item: {values.Item1}");
             Console.WriteLine($"Second item: {values.Item2}");
@@ -30,6 +48,9 @@ namespace FunWithTuples
             Console.WriteLine($"Int is: {samples.a}");
             Console.WriteLine($"String is: {samples.b}");
             Console.WriteLine($"Boolean is: {samples.c}");
+
+            Console.WriteLine($"X is: {pointValues.XPos}");
+            Console.WriteLine($"Y is: {pointValues.YPos}");
             Console.ReadLine();
         }
 
